66c9d72cfabbfb942cdf51361c4ee76c7403d3d3
Hancock <binhan.bai@thoughtworks.com>
2019-10-28 12:49:22 +0800

    [TECH] - 重构ParkingLot

--- src/main/java/tw/session/parkinglot/ParkingLot.java
+++ src/main/java/tw/session/parkinglot/ParkingLot.java
@@ -1,20 +1,24 @@
 package tw.session.parkinglot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
 
     private int capacity;
 
-    private Car car;
+    private List<Car> parkedCars;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        this.parkedCars = new ArrayList<>();
     }
 
     public Ticket park(Car car) {
-        if (capacity < 1 || this.car != null) {
+        if (parkedCars.size() >= capacity) {
             throw new ParkingLotFullException();
         }
-        this.car = car;
+        parkedCars.add(car);
         return new Ticket(car.getCarNum());
     }
 
@@ -22,12 +26,15 @@ public class ParkingLot {
         if (ticket.isNotValid()) {
             throw new InvalidTicketException();
         }
-        if (this.car != null && ticket.getCarNum().equals(this.car.getCarNum())) {
-            Car car = this.car;
-            this.car = null;
-            ticket.destroy();
-            return car;
-        }
-        throw new ParkingLotNotParkingTheCarException();
+
+        Car pickingCar = parkedCars
+            .stream()
+            .filter(car -> car.getCarNum().equals(ticket.getCarNum()))
+            .findFirst()
+            .orElseThrow(ParkingLotNotParkingTheCarException::new);
+
+        parkedCars.remove(pickingCar);
+        ticket.destroy();
+        return pickingCar;
     }
 }
